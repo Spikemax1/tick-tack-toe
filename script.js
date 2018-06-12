9b8c3c9665335ab2c748f67897156f0a66d2fae1
Spikemax1 <maxyxxx@mail.ru>
2018-06-12 19:04:19 +0500

    one

---
+++ script.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		boxes:[
+			{val: 'x', clicked: false, num: 1},
+			{val: 'o', clicked: false, num: 2},
+			{val: '', clicked: false, num: 3},
+			{val: '', clicked: false, num: 4},
+			{val: '', clicked: false, num: 5},
+			{val: '', clicked: false, num: 6},
+			{val: '', clicked: false, num: 7},
+			{val: '', clicked: false, num: 8},
+			{val: '', clicked: false, num: 9},
+		]
+	}
+
+})
