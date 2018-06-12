02c455efe99f69cf0086d27160082467c08228a8
Spikemax1 <maxyxxx@mail.ru>
2018-06-12 20:35:06 +0500

    game

--- script.js
+++ script.js
@@ -2,16 +2,37 @@ var app = new Vue({
 	el: '#app',
 	data: {
 		boxes:[
-			{val: 'x', clicked: false, num: 1},
-			{val: 'o', clicked: false, num: 2},
-			{val: '', clicked: false, num: 3},
-			{val: '', clicked: false, num: 4},
-			{val: '', clicked: false, num: 5},
-			{val: '', clicked: false, num: 6},
-			{val: '', clicked: false, num: 7},
-			{val: '', clicked: false, num: 8},
-			{val: '', clicked: false, num: 9},
-		]
+			{val: '', num: 0},
+			{val: '', num: 1},
+			{val: '', num: 2},
+			{val: '', num: 3},
+			{val: '', num: 4},
+			{val: '', num: 5},
+			{val: '', num: 6},
+			{val: '', num: 7},
+			{val: '', num: 8},
+		],
+		clicks: 0
+	},
+	methods:{
+		pick: function(num) {
+			if (this.clicks % 2 == 0) {
+				this.boxes[num].val = 'x';
+				this.clicks ++;				
+			}
+			else{
+				this.boxes[num].val = 'o';
+				this.clicks ++;	
+			}
+		},
+		resetVal: function(){
+			var arr = []			
+			for (var i = 0; i < this.boxes.length; i++) {
+				arr[i] = {val: '', num: i}
+			}
+			this.boxes = arr
+			this.clicks = 0
+		}
 	}
 
 })
